5f8f451e0d628bb00aab30dc9c13a761f7fd79fb
Dpalme <diegoPalmerin24@gmail.com>
2025-03-01 22:59:00 -0600

    updated flow

--- src/shared/components/socials.tsx
+++ src/shared/components/socials.tsx
@@ -34,7 +34,7 @@ export const SocialIcons = () => {
   const shouldReduceMotion = useReducedMotion();
   return (
     <m.div
-      className="fixed bottom-2 left-2 z-20 flex flex-col gap-4"
+      className="fixed bottom-4 right-4 z-20 flex flex-row gap-2"
       initial={{ y: shouldReduceMotion ? 0 : '100%', opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 1, delay: 0.3 }}
@@ -48,16 +48,6 @@ export const SocialIcons = () => {
         link="https://www.linkedin.com/in/diego-palmerin/"
         src={linkedinIcon}
         alt="linkedin"
-      />
-      <SocialLink
-        link="https://www.instagram.com/diegoo_pb2/"
-        src={instagramIcon}
-        alt="instagram"
-      />
-      <SocialLink
-        link="https://vsco.co/diego-pb/gallery"
-        src={vscoIcon}
-        alt="vsco"
       />
       <SocialLink
         link="mailto:dpalme@me.com"
